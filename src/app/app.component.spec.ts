36aad36d1ce548d0b02a52be24897924f31776a5
Mark aan t Goor <mark.aan.t.goor@alliander.com>
2018-11-01 13:10:10 +0100

    commentaar verwijderd

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,21 +19,3 @@ describe('AppComponent', () => {
   }));
 
 });
-
-
-// describe('Function: submitForm', () => {
-
-//   describe('when a valid "postcode" is submitted', () => {
-
-//     it('should assign the returned Planning object', () => {
-//       component.form.get('postcode').setValue('1001AA');
-//       component.submitForm();
-//       expect(component.planning).toEqual({
-//         aannemer: {
-//           naam: 'Energiewachtgroep',
-//           telefoonnummer: '038 770 01 96',
-//           url: 'http://www.meterafspraak.nl',
-//           bereikbaar: 'Op werkdagen tussen 8.00 uur en 21.00 uur'
-//         },
-//         kavelstatus: 'concept'
-//       });
